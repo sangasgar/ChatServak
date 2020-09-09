9b006d9d272acd0fb7a10b2a7cb581d3f6b37703
sangas@yandex.ru <papyangaregin>
2020-09-09 11:30:58 +0300

    Консольный сервер готов

--- src/Client.java
+++ src/Client.java
@@ -1,44 +1,36 @@
+import com.sun.org.omg.CORBA.Initializer;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Server {
-    private static int PORT = 8189;
-
+public class Client {
+    private static final int PORT = 8189;
+    private static final String IP_ADRESS = "localhost";
+    private static Socket socket;
     public static void main(String[] args) {
-        ServerSocket server = null;
-        Socket socket = null;
         try {
-            server = new ServerSocket(PORT);
-            System.out.println("Сервер запущен");
-            socket = server.accept();
-            System.out.println("Клиент подключился");
+            socket = new Socket(IP_ADRESS,PORT);
+            System.out.println("Подключился");
             Scanner in = new Scanner(socket.getInputStream());
             PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
             Scanner console = new Scanner(System.in);
             while (true) {
+
+                String inCons = in.nextLine();
+                System.out.println("Сервер: " + inCons);
                 System.out.println("Введите сообщение");
                 String cons = console.nextLine();
-                System.out.println("Сервер: " + cons);
                 if(cons.equals("/end")){
                     System.out.println("Клиент отключился");
                     break;
                 }
+                System.out.println("Клиент: " + cons);
                 out.println(cons);
-                System.out.println("Клиент: " + in.nextLine());
-
             }
-
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                server.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
